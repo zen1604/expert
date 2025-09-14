bd09bff73b7416b869205a274acb28516f8391e9
zen1604 <mahzenia@gmail.com>
2025-09-14 16:29:57 -0400

    Update page.js

--- app/admin/property/new/page.js
+++ app/admin/property/new/page.js
@@ -1,8 +1,10 @@
 // app/admin/property/new/page.js
 
-import PropertyForm from '../PropertyForm';
-import { upsertProperty } from '../actions';
-import styles from '../admin.module.css';
+// Corrected Paths: We are 2 levels deep, so we need to go up 2 times.
+import PropertyForm from '../../PropertyForm';
+import { upsertProperty } from '../../actions';
+import styles from '../../admin.module.css';
+
 import Link from 'next/link';
 
 export default function NewPropertyPage() {
